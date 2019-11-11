3653aa52570ba17c8101aac06a0f064e5fe50ca5
slidemorning <40626113+slidemorning@users.noreply.github.com>
2019-11-11 15:54:10 +0900

    Update stl_priority_queue.cpp

--- stl_priority_queue.cpp
+++ stl_priority_queue.cpp
@@ -9,6 +9,7 @@ std::priority_queue<pii, std::vector<pii>, std::less<pii>> max_pii_pq;
 std::priority_queue<pii, std::vector<pii>, std::greater<pii>> min_pii_pq;
 
 int main(void){
+    // time complexibility of method 'push' : O(logN)
     pq.push(5);
     pq.push(1);
     pq.push(9);
@@ -20,3 +21,7 @@ int main(void){
     }
     return 0;
 }
+
+/*
+    std output
+*/
