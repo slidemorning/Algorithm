1a4a819b88d5c8afebab8f4ade382d844144ac30
slidemorning <givertion2@gmail.com>
2020-10-17 18:34:38 +0900

    STL Set

---
+++ STL/stl-set-ex1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+template <typename T>
+void printSet(std::set<T>& s){
+    for(const auto& elem : s){
+        std::cout << elem << " " << std::endl;
+    }
+}
+
+class Todo{
+private:
+    int priority; // high value high priority
+    std::string job_desc;
+public:
+    Todo(int priority, std::string job_desc) : priority(priority), job_desc(job_desc) {}
+    bool operator<(const Todo& t) const {
+        if(priority == t.priority){
+            return job_desc < t.job_desc;
+        }
+        return priority > t.priority;
+    }
+    friend std::ostream& operator<<(std::ostream& o, const Todo& td);
+};
+
+std::ostream& operator<<(std::ostream& o, const Todo& td){
+    o << "[ priority : " << td.priority << "] " << td.job_desc;
+    return o;
+}
+
+int main(void){
+    std::set<Todo> todos;
+
+    todos.insert(Todo(1, "Running"));
+    todos.insert(Todo(2, "Study algorithm"));
+    todos.insert(Todo(1, "Vision project"));
+    todos.insert(Todo(3, "Meet friend"));
+    todos.insert(Todo(2, "Reading book"));
+
+    printSet(todos);
+
+    std::cout << "------------------" << std::endl;
+    std::cout << "If have done study algorithm" << std::endl;
+    todos.erase(todos.find(Todo(2, "Study algorithm")));
+
+    printSet(todos);
+}
---
+++ STL/stl-set.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <set>
+
+/*
+    1. set vs. multiset
+    Set (Do not allow duplication) vs Multi-Set(Allow duplication)
+
+    2. balanced bst structure
+    insert : log(N)
+    remove : log(N)
+    find   : log(N)
+
+    3. set vs. map
+    set : key 만 보관. 해당 key가 있는지 없는지 (True of False) 
+    map : key, value를 보관 (map으로 set을 구현 가능 메모리)
+    memory : map > set
+*/
+
+template <typename T>
+void printSet(std::set<T>& s){
+    std::cout << "[ ";
+    for(typename std::set<T>::iterator itr = s.begin(); itr != s.end(); itr++){
+        std::cout << *itr << " ";
+    }
+    std::cout << " ] " << std::endl;
+}
+
+int main(void){
+    std::set<int> s;
+    s.insert(10);
+    s.insert(50);
+    s.insert(20);
+    s.insert(40);
+    s.insert(30);
+
+    // 크기 순서대로 정렬되어 출력
+    std::cout << "print set ..." << std::endl;
+    printSet(s);
+
+    std::cout << "exist 20 ? :: ";
+    auto itr = s.find(20);
+    if(itr != s.end()){
+        std::cout << "Yes" << std::endl;
+    }else{
+        std::cout << "No" << std::endl;
+    }
+
+    std::cout << "exist 25 ? :: ";
+    itr = s.find(25);
+    if(itr != s.end()){
+        std::cout << "Yes" << std::endl;
+    }else{
+        std::cout << "No" << std::endl;
+    }
+}
